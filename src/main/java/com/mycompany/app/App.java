d74bfc2f0c25b9f2fc2b1aa5f0db231245defc37
Advait Lonkar <lonkaradvait@gmail.com>
2021-03-13 19:27:43 +0530

    Docker File

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,14 +10,17 @@ public class App
         Scanner scanner = new Scanner(System.in);
         double n1, n2;
         do{
+            // Scientific Calculator Terminal-based UI
             System.out.println("-------SCIENTIFIC CALCULATOR-------");
             System.out.println("MENU--");
             System.out.println("Press 1 to calculate SQUARE ROOT");
 
+            // User Entered Option
             int option;
             option = scanner.nextInt();
 
             switch(option){
+                // Square Root Option
                 case 1:
                     System.out.print("Enter the number to compute square root of: ");
                     n1 = scanner.nextDouble();
