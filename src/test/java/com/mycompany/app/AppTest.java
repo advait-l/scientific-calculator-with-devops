ba39e9421547502e6f89587b272d86d69c91cf3f
Advait Lonkar <lonkaradvait@gmail.com>
2021-03-17 21:14:27 +0530

    tests added

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,14 +1,62 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.*;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 public class AppTest 
 {
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    private App scientificCalculator;
+
+    @Before
+    public void setup(){
+        scientificCalculator = new App();
     }
+
+    // Square Root
+    @Test
+    public void squareRootTruePositive(){
+        assertEquals(scientificCalculator.sqrt(49), 7, 0.2f);
+    }
+
+    @Test
+    public void squareRootFalsePositive(){
+        assertNotEquals(scientificCalculator.sqrt(49), 5, 0.2f);
+    }
+
+    // Factorial
+    @Test
+    public void factorialTruePositive(){
+        assertEquals(scientificCalculator.fact(5), 120, 0.2f);
+    }
+
+    @Test
+    public void factorialFalsePositive(){
+        assertNotEquals(scientificCalculator.fact(4), 120, 0.2f);
+    }
+
+    // Natural Logarithm
+    @Test
+    public void naturalLogarithmTruePositive(){
+        assertEquals(scientificCalculator.nlog(1), 0, 0.2f);
+    }
+
+    @Test
+    public void naturalLogarithmFalsePositive(){
+        assertNotEquals(scientificCalculator.nlog(5), 10, 0.2f);
+    }
+
+    // Exponent
+    @Test
+    public void exponentTruePositive(){
+        assertEquals(scientificCalculator.exp(2, 3), 8, 0.2f);
+    }
+
+    @Test
+    public void exponentFalsePositive(){
+        assertNotEquals(scientificCalculator.exp(4, 3), 16, 0.2f);
+    }
+
+
 }
